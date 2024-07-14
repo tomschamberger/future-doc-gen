796924836356ea5c5db067104b7a28f89bcff31d
Tom Schamberger <tom@zukunft-verstehen.com>
2024-07-14 12:28:38 +0200

    Basic printable document created

---
+++ src/Document.jsx
@@ -0,0 +1,17 @@
+import './Document.css'
+
+function Document() {
+
+  return (
+    <div className="sheet-outer A4">
+    <section className="sheet padding-5mm">
+        <article>This is an A4 document.</article>
+    </section>
+    <section className="sheet padding-5mm">
+        <article>This is an A4 document.</article> 
+    </section>
+    </div>
+  )
+}
+
+export default Document
