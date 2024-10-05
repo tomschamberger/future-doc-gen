e2e8c921c398a57f4c1df80f4319a258bfb848e8
Tom Schamberger <tom.schamberger@msg.group>
2024-10-06 00:19:12 +0200

    Minor fixes

--- src/Document.jsx
+++ src/Document.jsx
@@ -2,15 +2,17 @@
 import {styled} from 'styled-components';
 
 const Main = styled.div`
-    
+    h1 {
+        color: red;
+    }
 `;
 
 const Document = () => {
   return (
     <Main>
-      <section data-ref="c1719f6d-9a79-489a-ae94-e7963ea84db5">
-        <h1 data-ref="724ebc30-efce-43bd-8ecb-a401d0bd3738">Lorem ipsum dolor sit amet</h1>
-        <p data-ref="3123b273-48d5-4b26-a006-623aff69ceb7">
+      <section>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non suscipit odio. Aenean ut ligula id mauris
           efficitur tincidunt vitae non leo. In convallis convallis leo, eget molestie metus imperdiet eu. Praesent
           pharetra, leo a laoreet mattis, ligula nisl commodo ante, non vestibulum nisi lacus eget magna. Maecenas
@@ -18,7 +20,7 @@ const Document = () => {
           vel sodales dolor, vel fringilla elit. Vivamus aliquam diam eu maximus elementum. Integer eu urna at felis
           fermentum hendrerit.
         </p>
-        <p data-ref="00ec9bd3-f7fc-4ded-a5aa-ecb2f0c72ec6">
+        <p>
           Nulla dignissim pellentesque magna ac maximus. Integer id tincidunt erat. Sed elementum posuere augue, quis
           pharetra mi vehicula in. Nullam rhoncus mi quis lectus gravida dignissim. Pellentesque a tortor ut leo pretium
           auctor non in massa. Nunc efficitur vestibulum mi, id mattis quam aliquet id. Ut semper tortor sit amet molestie
